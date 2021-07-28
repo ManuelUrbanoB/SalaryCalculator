13bf5cbc2384b7d734139cfef74bbd4ea54c7486
Manuel Urbano Benavides <manuelurbanobenavides@gmail.com>
2021-07-28 13:49:13 -0500

    first commit

---
+++ SalaryCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SalaryCalculator
+//
+//  Created by Manuel Alejandro Urbano Benavides on 28/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SalaryCalculator/SalaryCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalaryCalculatorApp.swift
+//  SalaryCalculator
+//
+//  Created by Manuel Alejandro Urbano Benavides on 28/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SalaryCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
